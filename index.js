23f4c818633d3e714b5cb054bd14d87e09cbec1e
YannDepardieu <yanndepardieu@outlook.fr>
2022-07-02 17:30:52 +0200

    Https setup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const debug = require('debug')('Express:Server');
-const http = require('http');
+// const http = require('http');
+
+const https = require('https');
+const fs = require('fs');
+
+const httpsOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
 require('dotenv').config();
 
 const app = require('./app');
@@ -10,12 +19,12 @@ const PORT = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
 console.log('process.env.NODE_ENV = ', process.env.NODE_ENV);
 
-const server = http.createServer(app);
+const server = https.createServer(httpsOptions, app);
 
 // I need my redis client to be connected to my http server before doing queries, so I use then. We use a pool
 // with pg, we could do the same but it's okay to let it like that. For a Client pg we should do it to.
 redisClient.connect().then(() => {
     server.listen(PORT, () => {
-        debug(`✅ Server listening on http://localhost:${PORT} ✅`);
+        debug(`✅ Server listening on https://localhost:${PORT} ✅`);
     });
 });
